ef2328028be8efae2b68b14476da526c657878b1
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-11-03 17:40:23 +0200

    Update ErrorMessage.jsx

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,5 +1,10 @@
+import PropTypes from 'prop-types';
 import { ErrorContainer } from "./ErrorMessage.styled"
 
 export const ErrorMessage = ({ text }) => {
 	return <ErrorContainer>{text}</ErrorContainer>
+}
+
+ErrorMessage.propTypes = {
+	text: PropTypes.string.isRequired,
 }
